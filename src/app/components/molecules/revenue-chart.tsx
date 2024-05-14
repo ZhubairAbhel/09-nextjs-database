2ef7d76cd11f90fe766fdcc910ffafa97c35af09
AkbarIhsan <93473539+AkbarIhsan@users.noreply.github.com>
2024-05-14 21:02:25 +0700

    p

--- src/app/components/molecules/revenue-chart.tsx
+++ src/app/components/molecules/revenue-chart.tsx
@@ -24,7 +24,7 @@ export default async function RevenueChart() {
             <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Recent Revenue
             </h2>
-            <div className="w-full rounded-xl bg-gray-50 p-4">
+            <div className="rounded-xl bg-gray-50 p-4">
                 <div className="mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
                     {/* y-axis */}
                     <div
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
           type="customers"
         />  */}
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <div className="w-full mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart /> 
         {/* <LatestInvoices /> */}
       </div>
